d9d1956d64e691d1c30ba91a245854ed1272ba5c
cedricdre <cedric.dreue@gmail.com>
2024-02-27 16:06:36 +0100

    Ajout script, css, img

---
+++ public/assets/js/script.js
@@ -0,0 +1,39 @@
+// Fonction - Masquer le header au scroll
+function hideHeaderScroll() {
+    let prevScrollPos = window.scrollY;
+    let header = document.querySelector('.header-hide');
+    let navbar = document.querySelector('.navbar');
+    let scrollThresholdNavbar = 300; // Ajuster la distance de déclenchement pour la navbar
+    let scrollThresholdBackground = 200; // Ajuster la distance de déclenchement pour le background
+
+    window.onscroll = function () {
+        let currentScrollPos = window.scrollY;
+
+        if (currentScrollPos > scrollThresholdNavbar) {
+            if (prevScrollPos > currentScrollPos) {
+                navbar.classList.remove('header-show');
+            } else {
+                navbar.classList.add('header-show');
+            }
+        } else {
+            navbar.classList.remove('header-show');
+        }
+
+        if (currentScrollPos > scrollThresholdBackground) {
+            if (prevScrollPos > currentScrollPos) {
+                header.classList.remove('header-show');
+            } else {
+                header.classList.add('header-show');
+                header.style.backgroundColor = 'transparent'; // Réinitialiser la couleur de fond
+            }
+        } else {
+            header.classList.remove('header-show');
+        }
+
+        prevScrollPos = currentScrollPos;
+    };
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    hideHeaderScroll();
+});
